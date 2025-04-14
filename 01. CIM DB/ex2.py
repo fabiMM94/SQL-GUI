a8f4204890c3c7c6751d70e303a512cf4a10061a
fabiMM94 <fabian.medina@coordinador.cl>
2025-04-14 10:17:32 -0400

    commit
    ejemplo

--- 01. CIM DB/ex2.py
+++ 01. CIM DB/ex2.py
@@ -38,36 +38,24 @@ usuario = "Fabi.fabi"
 contrasena = "Fabino123_#"
  
 params = urllib.parse.quote_plus(
-
     f"Driver={{ODBC Driver 17 for SQL Server}};"
-
     f"Server={SERVER_NAME};"
-
     f"Database={DB_NAME};"
-
     f"UID={usuario};"
-
     f"PWD={contrasena};"
 
 )
 
 connection_string = f"mssql+pyodbc:///?odbc_connect={params}"
- 
 engine = create_engine(connection_string, echo=True)
- 
 # Ejecutar query
-
 try:
     print("Comienza la query")
     query = "SELECT * FROM CompanyT"
-
     df = pd.read_sql(query, engine)
-
     print("✅ Datos extraídos con éxito:")
-
     print(df.head())
 
 except Exception as e:
-
     print(f"❌ Error al ejecutar la query: {e}")
  
